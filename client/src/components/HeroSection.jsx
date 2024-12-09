887a0d497583dd263a21bd7be03b32ecb2b36dc6
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-12-09 14:00:32 +0100

    Createa heroSection component

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ function HeroSection() {
       <div className="hero-content">
         <div className="text-content">
           <h1>
-            Discover the Joy of <span className="highlight">Reading</span> with <strong>Book Haven</strong>
+            Discover the Joy of <span className="highlight1">Reading</span> with <strong className="highlight2">Book Haven</strong>
           </h1>
           <p>
             Welcome to Book Haven, where we believe in the power of books to entertain, inspire, and transform lives. Explore our collection and embark on a journey of imagination and knowledge.
@@ -20,11 +20,15 @@ function HeroSection() {
           <img src={heroImage} alt="Person reading a book" />
         </div>
       </div>
+      <img src="/stars1.png" alt="Star" className="star star1" />
+      <img src="/stars2.png" alt="Star" className="star star2" />
+      <img src="/stars3.png" alt="Star" className="star star3" />
+
       <div className="community-section">
         <div className="avatars">
-          <img src="./assets/avatar1.png" alt="User 1" />
-          <img src="./assets/avatar2.png" alt="User 2" />
-          <img src="./assets/avatar3.png" alt="User 3" />
+          <img src="/avatar1.png" alt="User 1" />
+          <img src="/avatar2.png" alt="User 2" />
+          <img src="/avatar3.png" alt="User 3" />
         </div>
         <p>50K+ Book lovers joined</p>
       </div>
