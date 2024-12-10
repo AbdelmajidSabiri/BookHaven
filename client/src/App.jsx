245f9e904ce890c70b7aaf55a437b21441fd80d5
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-12-10 11:45:00 +0100

    Turn NavBar like a component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,13 @@
 import './App.css';
 import HeroSection from './components/HeroSection';
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
 
     <div className="container">
-      <nav>
-        <div className='navbar'>
-          <img src="/logoNav.png"  alt="BookHaven Logo" className="logo" />
-          <ul className='navlinks'>
-            <li><a href="">Home</a></li>
-            <li><a href="">Books</a></li>
-            <li><a href="">Share Book</a></li>
-            <li><a href="">More</a></li>
-          </ul>
-          <button className="btn">Sign In</button>
-        </div>
-      </nav>
-
+      <NavBar/>
       <HeroSection />
     </div>
   )
