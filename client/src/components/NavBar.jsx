e55cda06b12cad00ffed0b4ab1422bffa8dd185e
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-12-10 11:45:18 +0100

    Add navbar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+
+function NavBar(){
+    return(
+        <nav>
+            <div className='navbar'>
+            <img src="/logoNav.png"  alt="BookHaven Logo" className="logo" />
+            <ul className='navlinks'>
+                <li><a href="">Home</a></li>
+                <li><a href="">Books</a></li>
+                <li><a href="">Share Book</a></li>
+                <li><a href="">More</a></li>
+            </ul>
+            <button className="btn">Sign In</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
